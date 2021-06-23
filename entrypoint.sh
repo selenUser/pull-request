832a4403e46f4151803abee47dd10c8a8f73a704
arie.hoenel <arie.hoenel@ironsrc.com>
2021-06-23 13:23:44 +0300

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,6 +36,13 @@ else
   echo "branches[1] = ${branches[1]}"
   DESTINATION_BRANCH="{$branches}"
   echo "DESTINATION_BRANCH = $DESTINATION_BRANCH"
+  for branch in ${branches} ; do
+    echo "branch = ${branch}"
+    if [[ "${branch}" != "${INPUT_DESTINATION_BRANCH_REGEX}" ]] && [[ "${branch}" != *"\*"* ]] && [[ "${branch}" != remote* ]]; then
+      run_command "$branch"
+    fi
+  done
+
 fi
 
 if [ "$(git rev-parse --revs-only "$SOURCE_BRANCH")" = "$(git rev-parse --revs-only "$DESTINATION_BRANCH")" ]; then
@@ -54,59 +61,61 @@ fi
 # Workaround for `hub` auth error https://github.com/github/hub/issues/2149#issuecomment-513214342
 export GITHUB_USER="$GITHUB_ACTOR"
 
-PR_ARG="$INPUT_PR_TITLE"
-if [[ ! -z "$PR_ARG" ]]; then
-  PR_ARG="-m \"$PR_ARG\""
+#PR_ARG="$INPUT_PR_TITLE"
+#if [[ ! -z "$PR_ARG" ]]; then
+#  PR_ARG="-m \"$PR_ARG\""
+#
+#  if [[ ! -z "$INPUT_PR_TEMPLATE" ]]; then
+#    sed -i 's/`/\\`/g; s/\$/\\\$/g' "$INPUT_PR_TEMPLATE"
+#    PR_ARG="$PR_ARG -m \"$(echo -e "$(cat "$INPUT_PR_TEMPLATE")")\""
+#  elif [[ ! -z "$INPUT_PR_BODY" ]]; then
+#    PR_ARG="$PR_ARG -m \"$INPUT_PR_BODY\""
+#  fi
+#fi
+#
+#if [[ ! -z "$INPUT_PR_REVIEWER" ]]; then
+#  PR_ARG="$PR_ARG -r \"$INPUT_PR_REVIEWER\""
+#fi
+#
+#if [[ ! -z "$INPUT_PR_ASSIGNEE" ]]; then
+#  PR_ARG="$PR_ARG -a \"$INPUT_PR_ASSIGNEE\""
+#fi
+#
+#if [[ ! -z "$INPUT_PR_LABEL" ]]; then
+#  PR_ARG="$PR_ARG -l \"$INPUT_PR_LABEL\""
+#fi
+#
+#if [[ ! -z "$INPUT_PR_MILESTONE" ]]; then
+#  PR_ARG="$PR_ARG -M \"$INPUT_PR_MILESTONE\""
+#fi
+#
+#if [[ "$INPUT_PR_DRAFT" ==  "true" ]]; then
+#  PR_ARG="$PR_ARG -d"
+#fi
 
-  if [[ ! -z "$INPUT_PR_TEMPLATE" ]]; then
-    sed -i 's/`/\\`/g; s/\$/\\\$/g' "$INPUT_PR_TEMPLATE"
-    PR_ARG="$PR_ARG -m \"$(echo -e "$(cat "$INPUT_PR_TEMPLATE")")\""
-  elif [[ ! -z "$INPUT_PR_BODY" ]]; then
-    PR_ARG="$PR_ARG -m \"$INPUT_PR_BODY\""
+run_command(){
+  COMMAND="hub pull-request \
+    -b $1 \
+    -h $SOURCE_BRANCH \
+    --no-edit \
+    $PR_ARG \
+    || true"
+
+  echo "$COMMAND"
+
+  PR_URL=$(sh -c "$COMMAND")
+  if [[ "$?" != "0" ]]; then
+    exit 1
   fi
-fi
 
-if [[ ! -z "$INPUT_PR_REVIEWER" ]]; then
-  PR_ARG="$PR_ARG -r \"$INPUT_PR_REVIEWER\""
-fi
-
-if [[ ! -z "$INPUT_PR_ASSIGNEE" ]]; then
-  PR_ARG="$PR_ARG -a \"$INPUT_PR_ASSIGNEE\""
-fi
-
-if [[ ! -z "$INPUT_PR_LABEL" ]]; then
-  PR_ARG="$PR_ARG -l \"$INPUT_PR_LABEL\""
-fi
-
-if [[ ! -z "$INPUT_PR_MILESTONE" ]]; then
-  PR_ARG="$PR_ARG -M \"$INPUT_PR_MILESTONE\""
-fi
-
-if [[ "$INPUT_PR_DRAFT" ==  "true" ]]; then
-  PR_ARG="$PR_ARG -d"
-fi
-
-COMMAND="hub pull-request \
-  -b $DESTINATION_BRANCH \
-  -h $SOURCE_BRANCH \
-  --no-edit \
-  $PR_ARG \
-  || true"
-
-echo "$COMMAND"
-
-PR_URL=$(sh -c "$COMMAND")
-if [[ "$?" != "0" ]]; then
-  exit 1
-fi
-
-echo ${PR_URL}
-# shellcheck disable=SC2082
-echo "::set-output name=destination_branch::${DESTINATION_BRANCH}"
-echo "::set-output name=pr_url::${PR_URL}"
-echo "::set-output name=pr_number::${PR_URL##*/}"
-if [[ "$LINES_CHANGED" = "0" ]]; then
-  echo "::set-output name=has_changed_files::false"
-else
-  echo "::set-output name=has_changed_files::true"
-fi
+  echo ${PR_URL}
+  # shellcheck disable=SC2082
+  echo "::set-output name=destination_branch::${DESTINATION_BRANCH}"
+  echo "::set-output name=pr_url::${PR_URL}"
+  echo "::set-output name=pr_number::${PR_URL##*/}"
+  if [[ "$LINES_CHANGED" = "0" ]]; then
+    echo "::set-output name=has_changed_files::false"
+  else
+    echo "::set-output name=has_changed_files::true"
+  fi
+}
